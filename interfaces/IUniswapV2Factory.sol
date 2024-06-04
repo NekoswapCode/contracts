3af10d22673b9e63e58c90971c7ab12e91eb3e8d
neko <lubu.manta@gmail.com>
2024-06-04 15:23:20 +0800

    remove migrator

--- interfaces/IUniswapV2Factory.sol
+++ interfaces/IUniswapV2Factory.sol
@@ -8,7 +8,6 @@ interface IUniswapV2Factory {
 
     function feeTo() external view returns (address);
     function feeToSetter() external view returns (address);
-    function migrator() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint) external view returns (address pair);
